af647a6ef0aef177c98bb4d6d7f44dd1725f11a2
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2020-07-30 20:06:35 +0300

    Added DummyController

--- NullHttpContextRepro.Tests/HttpContextTest.cs
+++ NullHttpContextRepro.Tests/HttpContextTest.cs
@@ -1,5 +1,8 @@
+using System.Net.Http;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
@@ -21,5 +24,32 @@ namespace NullHttpContextRepro.Tests
             // assert
             httpContext.Should().NotBeNull();
         }
+
+        [Fact]
+        public async Task TestHttpContextWithDummyController()
+        {
+            // arrange
+            // need to integrate DummyController into the website
+            using var app = new WebApplicationFactory<Startup>();
+            HttpClient client = app.CreateClient();
+
+            // act
+            HttpResponseMessage response = await client.GetAsync("dummy");
+            string content = await response.Content.ReadAsStringAsync();
+
+            // assert
+            response.IsSuccessStatusCode.Should().BeTrue();
+            content.Should().Be(DummyController.SuccessMessage);
+        }
+
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class DummyController : Controller
+    {
+        public const string SuccessMessage = nameof(SuccessMessage);
+        [HttpGet]
+        public string Get() => SuccessMessage;
     }
 }
